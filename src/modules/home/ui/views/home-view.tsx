ad740fd4eb702f71b7c782e52712068c032bd3a4
Naman garg <naman.2327cseml1022@example.com>
2025-06-30 03:19:29 +0530

    TRPC setup

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,32 +1,16 @@
 "use client"
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { authClient } from '@/lib/auth-client'
-import { ro } from 'date-fns/locale'
-import React, { useState } from 'react'
-import { useRouter } from "next/navigation"
-export const HomeView = () => {
-    const { 
-        data: session } = authClient.useSession() 
-    const router = useRouter();
- if(!session) {
-  return (
-<p>loagin...</p>
-  )}
- 
-     return (
-      <div>
-      <p>logged in as {session.user.name}</p>
-      <Button onClick={()=>{
-        authClient.signOut({
-            fetchOptions: {
-                onSuccess: () =>  router.push("/sign-in") ,
-            }
-        })
-      }} >sign out </Button>
-      </div>
-     )
 
-  
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+
+export const HomeView = () => {
+const trpc = useTRPC();
+const {data} = useQuery(trpc.hello.queryOptions({text:"Naman"}));
+
+  return (
+    <div>
+      <h1>{data?.greeting}</h1>
+    </div>
+  );
 }
 
